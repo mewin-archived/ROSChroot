bba23b75ab55ed3ee14951d9e591e881800045d8
Patrick Wuttke <mewin@mewin.de>
2018-04-26 12:10:23 +0200

    fixed X connection not working on my arch machine

--- enter_chroot.sh
+++ enter_chroot.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 if [ `id -u` -ne 0 ] ; then
-	sudo bash "$0" "$@"
+	sudo bash "$0" "$HOME/.Xauthority"
 	exit
 fi
 
-xhost +localhost
+if [ -n "$1" ] ; then
+    cp "$1" "/opt/debian/home/user/.Xauthority"
+fi
+
+xhost +
 source _chroot_prelude.sh
 
 # chroot /opt/debian sudo -u user /bin/bash

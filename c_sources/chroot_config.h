0dc4cf09e3d043ef0272582911546e21925f3aaa
Patrick Wuttke <mewin@mewin.de>
2018-05-04 01:35:03 +0200

    removed my generated config

--- c_sources/chroot_config.h
+++
@@ -1,7 +0,0 @@
-#define CHROOT_ROOT "/opt/debian"
-#define CHROOT_USER "user"
-#define MOUNT "/usr/bin/mount"
-#define UMOUNT "/usr/bin/umount"
-#define CHROOT "/usr/bin/chroot"
-#define TMUX "/usr/bin/tmux"
-#define TMUX_CONF "/media/Daten/Projekte/ROSChroot/tmux.conf"
